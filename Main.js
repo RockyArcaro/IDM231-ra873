c604dcb8260700084342e8b8b048339e89c01bfc
RockyArcaro <ra873@drexel.edu>
2023-03-07 13:11:28 -0500

    Fixed Bugs

--- Main.js
+++ Main.js
@@ -2,6 +2,10 @@ function dateSubmission(){
   let date = document.getElementById('birthday').value;
   let monthStr = date.substr(5, 2);
   let dayStr = date.substr(8, 2); 
+  let audioPlayer = null
+  let message = ""
+
+  const CapricornButton = document.getElementById('capricorn'); 
 
   let month = parseInt(monthStr);
   let day = parseInt(dayStr);
@@ -31,15 +35,103 @@ function dateSubmission(){
   } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
     astrological_sign = 'Aquarius'
   }
-let resultImg = document.getElementById('resultImg');
-if(astrological_sign == 'Capricorn'){
-  resultImg.src = "../Photos/AJBrown.jpg" 
-}
+
+  let resultImg = document.getElementById('resultImg');
+  if (astrological_sign == 'Capricorn'){
+    resultImg.src = "./Photos/AJBrown.jpg" 
+  } else if(astrological_sign == 'Sagittarius'){
+    resultImg.src = "./Photos/TyreseMaxey.jpg"
+  } else if(astrological_sign == 'Scorpio'){
+    resultImg.src = "./Photos/NickCastellanos.jpg"  
+    soundPlayer = new Audio("")
+    message = "You're Horny"
+  } else if(astrological_sign == 'Libra'){
+    resultImg.src = "./Photos/MilesSanders.jpg"  
+  } else if(astrological_sign == 'Virgo'){
+    resultImg.src = "./Photos/MatisseThybulle.jpg"  
+  } else if(astrological_sign == 'Leo'){
+    resultImg.src = "./Photos/KyleShwarber.jpg"  
+  } else if(astrological_sign == 'Cancer'){
+    resultImg.src = "./Photos/JoelEmbiid.jpg"  
+  } else if(astrological_sign == 'Gemini'){
+    resultImg.src = "./Photos/JasonKelce.jpg"  
+  } else if(astrological_sign == 'Taurus'){
+    resultImg.src = "./Photos/JamesHarden.jpg"  
+  } else if(astrological_sign == 'Aries'){
+    resultImg.src = "./Photos/JalenHurts.jpg"  
+  } else if(astrological_sign == 'Pisces'){
+    resultImg.src = "./Photos/BryceHarper.jpg"  
+  } else if(astrological_sign == 'Aquarius'){
+    resultImg.src = "./Photos/BrandonMarsh.jpg"  
+  }
+
+  audioPlayer.play()
 
   console.log(astrological_sign);
-  
   console.log(date);
 }
+
 let astrological_sign='sign';
 let birthday = document.getElementById('submit');
 birthday.addEventListener('click', () => {dateSubmission();});
+
+const capricornButton = document.getElementById('Capricornus');
+capricornButton.addEventListener('click', () => {
+  astrological_sign = 'Capricorn'
+  dateSubmission();
+});
+const SagittariusButton = document.getElementById('Saggitarius');
+SagittariusButton.addEventListener('click', () => {
+  astrological_sign = 'Sagittarius'
+  dateSubmission();
+});
+const ScorpioButton = document.getElementById('Scorpio');
+ScorpioButton.addEventListener('click', () => {
+  astrological_sign = 'Scorpio'
+  dateSubmission();
+});
+const LibraButton = document.getElementById('Libra');
+LibraButton.addEventListener('click', () => {
+  astrological_sign = 'Libra'
+  dateSubmission();
+});
+const VirgoButton = document.getElementById('Virgo');
+VirgoButton.addEventListener('click', () => {
+  astrological_sign = 'Virgo'
+  dateSubmission();
+});
+const LeoButton = document.getElementById('Leo');
+LeoButton.addEventListener('click', () => {
+  astrological_sign = 'Leo'
+  dateSubmission();
+});
+const CancerButton = document.getElementById('Cancer');
+CancerButton.addEventListener('click', () => {
+  astrological_sign = 'Cancer'
+  dateSubmission();
+});
+const GeminiButton = document.getElementById('Gemini');
+GeminiButton.addEventListener('click', () => {
+  astrological_sign = 'Gemini'
+  dateSubmission();
+});
+const TaurusButton = document.getElementById('Taurus');
+TaurusButton.addEventListener('click', () => {
+  astrological_sign = 'Taurus'
+  dateSubmission();
+});
+const AriesButton = document.getElementById('Aries');
+AriesButton.addEventListener('click', () => {
+  astrological_sign = 'Aries'
+  dateSubmission();
+});
+const PiscesButton = document.getElementById('Pisces');
+PiscesButton.addEventListener('click', () => {
+  astrological_sign = 'Pisces'
+  dateSubmission();
+});
+const AquariusButton = document.getElementById('Aquarius');
+AquariusButton.addEventListener('click', () => {
+  astrological_sign = 'Aquarius'
+  dateSubmission();
+});
